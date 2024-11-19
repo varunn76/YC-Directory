a95340a6dfe50dfc715edf7c8acad7f443a17b7b
varun <vk465396@gmail.com>
2024-11-20 00:22:25 +0530

    startup page created

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     dangerouslyAllowSVG: true,
     remotePatterns: [{ protocol: 'https', hostname: '*' }],
   },
+  experimental: {
+    ppr: 'incremental',
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right',
+  },
 };
 
 export default nextConfig;
