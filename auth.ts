120fc219906f0bdd2e46314e079b4e92be4745b5
varun <vk465396@gmail.com>
2024-12-08 17:32:22 +0530

    hchd

--- auth.ts
+++ auth.ts
@@ -1,4 +1,7 @@
-import NextAuth from 'next-auth';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint no-use-before-define: 0 */ // --> OFF
+
+import NextAuth, { Profile } from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import { client } from '@/sanity/lib/client';
 import { writeClient } from '@/sanity/lib/write-client';
@@ -8,29 +11,41 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
     async signIn({
-      user: { name, email, image },
-      profile: { id, login, bio },
-    }) {
+      user,
+      profile,
+    }: {
+      user: any;
+      profile?: Profile | undefined;
+    }): Promise<boolean | string> {
+      // Ensure profile exists if it's required
+      if (!profile || !profile.id) {
+        console.error('Profile or Profile ID is missing');
+        return false; // Deny sign-in if profile or profile.id is not available
+      }
+
+      const id = profile.id; // Access safely after checking existence
+      const login = profile.login || '';
+      const bio = profile.bio || '';
+
       const existingUser = await client
         .withConfig({ useCdn: false })
-        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
-          id,
-        });
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
 
       if (!existingUser) {
         await writeClient.create({
           _type: 'author',
           id,
-          name,
+          name: user.name || '',
           username: login,
-          email,
-          image,
-          bio: bio || '',
+          email: user.email || '',
+          image: user.image || '',
+          bio,
         });
       }
 
-      return true;
+      return true; // Allow sign-in
     },
+
     async jwt({ token, account, profile }) {
       if (account && profile) {
         const user = await client
--- components/SeachFormReset.tsx
+++ components/SeachFormReset.tsx
@@ -16,5 +16,5 @@ const SearchFormReset = () => {
     </button>
   );
 };
-``;
+
 export default SearchFormReset;
